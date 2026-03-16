6f552dcb689f676c744ed3ef89f5214bf4487f58
Astraclicker <2867314230@qq.com>
2026-03-16 14:06:21 +0800

    规范java程序入口

--- JavaCoreTechnology1/hello_world.java
+++ JavaCoreTechnology1/hello_world.java
@@ -1,4 +1,5 @@
-void main()
-{
-    System.out.println("hello world");
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
